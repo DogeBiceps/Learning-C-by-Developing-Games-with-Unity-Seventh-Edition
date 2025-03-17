596a68d7c44e48a3cf0898676c6a8e3932567e86
DogeBiceps <150923592+DogeBiceps@users.noreply.github.com>
2025-03-17 15:17:44 +0100

    Hashsets
    Hashsets assignment

---
+++ HashSets/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS_Using_HashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using HashSet");
+            //1. Defining String Array (Note that the string "mahesh" is repeated) 
+            string[] names = new string[] {
+                "mahesh",
+                "vikram",
+                "mahesh",
+                "mayur",
+                "suprotim",
+                "saket",
+                "manish"
+            };
+            //2. Length of Array and Printing array
+            Console.WriteLine("Length of Array " + names.Length);
+            Console.WriteLine();
+            Console.WriteLine("The Data in Array");
+            foreach (var n in names)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine();
+            //3. Defining HashSet by passing an Array of string to it
+            HashSet<string> hSet = new HashSet<string>(names);
+            //4. Count of Elements in HashSet
+            Console.WriteLine("Count of Data in HashSet " + hSet.Count);
+            Console.WriteLine();
+            //5. Printing Data in HashSet, this will eliminate duplication of "mahesh" 
+            Console.WriteLine("Data in HashSet");
+            foreach (var n in hSet)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadLine();
+        }
+    }
+}
